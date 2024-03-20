7434e1f68ddc56b72b10ff7aa74f9bac9ad403ab
levg34 <levg34@users.noreply.github.com>
2024-03-20 20:58:13 +0000

    display user on page

--- src/components/UserPage.svelte
+++ src/components/UserPage.svelte
@@ -1,1 +1,15 @@
-<h2>USER</h2>
+<script lang="ts">
+  import { UserFactory } from '../ts/user'
+  import User from './user/User.svelte'
+
+  //   const factory: UserFactory = await UserFactory.init()
+  const userPromise = UserFactory.init().then((factory) => {
+    return factory.createUser()
+  })
+</script>
+
+{#await userPromise}
+  <p>Creating user...</p>
+{:then user}
+  <User {user} />
+{/await}
---
+++ src/components/user/User.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { IUser } from '../../ts/user'
+
+  export let user: IUser
+</script>
+
+<div class="user-card">
+  <img class="avatar" src={user.avatar} alt={`Avatar of ${user.firstName}`} />
+  <div class="user-info">
+    <h2>{user.firstName} {user.lastName}</h2>
+    <p>Email: {user.email}</p>
+    <p>Birthday: {user.birthday.toDateString()}</p>
+    <p>Sex: {user.sex}</p>
+  </div>
+</div>
+
+<style>
+  .user-card {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 1rem;
+    margin: 1rem;
+    display: flex;
+    align-items: center;
+  }
+  .user-info {
+    margin-left: 1rem;
+  }
+  .avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+</style>
